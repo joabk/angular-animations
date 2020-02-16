1159d82f83c0bd2936842e4a063cceedf8442de2
joabk <mjoab88@gmail.com>
2020-02-16 20:50:10 +0300

    3 files modified

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,5 +1,16 @@
 import { trigger, state, transition, animate, style } from '@angular/animations';
 
+export let slide = trigger('slide',[
+  transition(':enter',[
+    style({transform:'translateX(-10px)'}),
+    animate(2000)
+  ]),
+
+  transition(':leave',[
+    animate(2000, style({transform:'translateX(-100%)'}))
+  ])
+]);
+
 export let fade = trigger('fade', [
 
       state('void', style({opacity:0})),
